f865518dc89c6051f928ca042a0ae20e90a7ea0b
surfiniaburger <jdmasciano2@gmail.com>
2026-04-17 00:36:16 +0100

    google

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: "https://in-varia.com/sitemap.xml",
+    };
+}
